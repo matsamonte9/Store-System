cf3bc4f50b36b3d97ed69e818487dc308a98d8ba
matsamonte9 <mat.samonte9@gmail.com>
2026-01-20 23:37:43 +0800

    back to square one

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const serverless = require('serverless-http');
 
 // router
 const authRouter = require('./routes/auth');
@@ -41,33 +40,15 @@ app.use('/api/v1/cart', authenticateUser, authorizedPermission('admin', 'cashier
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Server is alive!');
-});
-
-let isConnected = false;
-const connect = async () => {
-  if (!isConnected) {
+const start = async () => {
+  try {
     await connectDB(process.env.MONGO_URI);
-    console.log('Connected to DB');
-    isConnected = true;
+    app.listen(port, '0.0.0.0', console.log(`Server Listening on Port: ${port}`));
+  } catch (error) {
+    console.log(error);
   }
-};
-connect().catch(err => console.log('DB connection error:', err));
+}
 
-// Export for Vercel
-module.exports = app;
-module.exports.handler = serverless(app);
-
-// const start = async () => {
-//   try {
-//     await connectDB(process.env.MONGO_URI);
-//     // app.listen(port, '0.0.0.0', console.log(`Server Listening on Port: ${port}`));
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// start();
+start();
